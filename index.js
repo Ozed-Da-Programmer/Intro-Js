6bef2a14df79b61ea59bba9ea86a4bb407c6d496
Ozed-Da-Programmer <tonyrich0729@gmail.com>
2026-02-14 13:07:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Osaze";
+const age = 17;
+const favoriteLanguage = "Python";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
